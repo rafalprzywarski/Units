a3bd6a785d0441ef0a336ee6259fc71de0592c05
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-11-03 21:55:47 +0100

    sim main loop

--- source/sim/main.cpp
+++ source/sim/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "everything OK!" << std::endl;
+    sf::Window window(sf::VideoMode(800, 600), "units", sf::Style::Close | sf::Style::Titlebar);
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+    }
 }
