9073d991799811df0f5df7de980e1044e195ea49
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-06 14:35:37 +0100

    one unit linear walking

--- source/sim/walking.cpp
+++ source/sim/walking.cpp
@@ -15,64 +15,22 @@ struct walking_unit
 
 using walking_units = std::vector<walking_unit>;
 
-ams::units create_units_grid(ams::vec2f origin, ams::vec2i size, float radius, float spacing)
+void update_unit(walking_unit& u)
 {
-    ams::units group;
-    for (int i = 0; i < size[0]; ++i)
-        for (int j = 0; j < size[1]; ++j)
-            group.push_back(ams::unit{
-                radius,
-                origin +
-                ams::vec2f{
-                    spacing + radius + i * (radius * 2 + spacing),
-                    spacing + radius + j * (radius * 2 + spacing)
-                }});
-    return group;
-}
-
-bool collides(const ams::unit& u, const ams::units& group)
-{
-    for (auto& other : group)
-        if (&u != &other && cml::length(u.position - other.position) < (u.radius + other.radius))
-            return true;
-    return false;
-}
-
-void update_units(ams::units& group)
-{
-    for (auto& u : group)
-    {
-        auto old_position = u.position;
-        auto direction = u.target_position - u.position;
-        if (length(direction) == 0)
-            continue;
-        u.position += normalize(direction) * (2 + std::rand() % 10 * 0.2f);
-        if (!collides(u, group))
-            continue;
-
-        u.position = old_position;
-
-        u.position[0] += std::rand() % 5 - 2;
-        u.position[1] += std::rand() % 5 - 2;
-        if (collides(u, group))
-            u.position = old_position;
-    }
-}
-
-void set_units_target(ams::units& group, ams::vec2f target)
-{
-    for (auto& u : group)
-        u.target_position = target;
+    auto direction = u.target_position - u.position;
+    auto distance = length(direction);
+    if (distance < 0.5)
+        return;
+    u.position += direction * std::min(1.0f, (2 + std::rand() % 10 * 0.2f) / distance);
 }
 
 int main()
 {
     auto window = ams::gui::create_window();
-    auto group = create_units_grid({5, 5}, {30, 30}, 5, 7);
+    walking_unit unit{10, {0, 0}};
 
     ams::gui::main_loop(*window, [&](float world_x, float world_y)
     {
-        ams::box_formation formation{{1, 0}, {world_x, world_y}, 1000, 25};
-        assign_formation(formation, group);
-    }, [&] { update_units(group); }, [&] { ams::gui::draw_units(group, *window); } );
+        unit.target_position = {world_x, world_y};
+    }, [&] { update_unit(unit); }, [&] { ams::gui::draw_units(walking_units{unit}, *window); } );
 }
