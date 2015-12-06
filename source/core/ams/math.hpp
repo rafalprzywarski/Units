ab31f1696a131b12d06229c8b4aa758f60e33208
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-06 15:00:30 +0100

    drawing feet according to direction

--- source/core/ams/math.hpp
+++ source/core/ams/math.hpp
@@ -7,4 +7,9 @@ namespace ams
 using vec2f = cml::vector2f;
 using vec2i = cml::vector2i;
 
+inline vec2f cross_product(const vec2f& v)
+{
+    return {-v[1], v[0]};
+}
+
 }
--- source/sim/walking.cpp
+++ source/sim/walking.cpp
@@ -9,6 +9,7 @@ struct walking_unit
     float feet_spacing = 0;
     ams::vec2f position;
     ams::vec2f target_position;
+    ams::vec2f direction{0, 1};
 
     walking_unit() = default;
     walking_unit(float radius, float feet_spacing, ams::vec2f position) : radius(radius), feet_spacing(feet_spacing), position(position), target_position(position) { }
@@ -22,6 +23,7 @@ void update_unit(walking_unit& u)
     auto distance = length(direction);
     if (distance < 0.5)
         return;
+    u.direction = direction / distance;
     u.position += direction * std::min(1.0f, (2 + std::rand() % 10 * 0.2f) / distance);
 }
 
@@ -43,9 +45,11 @@ void draw_walking_units(const unit_container& group, sf::RenderWindow& window)
         window.draw(shape);
         float foot_radius = FOOT_RATIO * u.radius;
         shape.setRadius(foot_radius);
-        shape.setPosition(u.position[0] - u.feet_spacing * 0.5f - foot_radius, u.position[1] - foot_radius);
+        auto left_foot = ams::cross_product(u.direction) * (u.feet_spacing * 0.5f);
+        auto right_foot = -left_foot;
+        shape.setPosition(u.position[0] + left_foot[0] - foot_radius, u.position[1] + left_foot[1] - foot_radius);
         window.draw(shape);
-        shape.setPosition(u.position[0] + u.feet_spacing * 0.5f - foot_radius, u.position[1] - foot_radius);
+        shape.setPosition(u.position[0] + right_foot[0] - foot_radius, u.position[1] + right_foot[1] - foot_radius);
         window.draw(shape);
     }
 }
