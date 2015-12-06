769b26992aeb23f5b2749e8ae41d434d9dd60748
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-06 14:14:18 +0100

    box uses units::guy

--- source/sim/box.cpp
+++ source/sim/box.cpp
@@ -1,7 +1,6 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
 #include <units/unit.hpp>
 #include <units/formations.hpp>
+#include <units/gui/window.hpp>
 #include <memory>
 
 units::units create_units_grid(units::vec2f origin, units::vec2i size, float radius, float spacing)
@@ -54,105 +53,13 @@ void set_units_target(units::units& group, units::vec2f target)
         u.target_position = target;
 }
 
-void draw_units(const units::units& group, sf::RenderWindow& window)
-{
-    sf::CircleShape shape(1, 16);
-    shape.setFillColor(sf::Color(0, 0, 0, 0));
-    float thickness = 2;
-    shape.setOutlineThickness(thickness);
-    shape.setOutlineColor(sf::Color(150, 250, 100));
-
-    for (auto& u : group)
-    {
-        shape.setRadius(u.radius - thickness);
-        shape.setPosition(u.position[0] - u.radius, u.position[1] - u.radius);
-        window.draw(shape);
-    }
-}
-
-std::unique_ptr<sf::RenderWindow> create_window()
-{
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 16;
-    auto window = std::make_unique<sf::RenderWindow>(sf::VideoMode(800, 600), "units", sf::Style::Default, settings);
-    window->setVerticalSyncEnabled(true);
-    return window;
-}
-
-class view_controller
-{
-public:
-    sf::View get_view(sf::Vector2u window_size)
-    {
-        return {pan, sf::Vector2f(window_size)};
-    }
-    void pan_x(float d)
-    {
-        pan.x -= d;
-    }
-    void pan_y(float d)
-    {
-        pan.y -= d;
-    }
-private:
-    sf::Vector2f pan{0, 0};
-};
-
-template <typename F, typename EH>
-void main_loop(sf::RenderWindow& window, EH handler, F step)
-{
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            handler(event);
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-
-        step();
-
-        window.display();
-    }
-}
-
 int main()
 {
-    auto window = create_window();
+    auto window = units::gui::create_window();
     auto group = create_units_grid({5, 5}, {30, 30}, 5, 7);
-    view_controller main_view;
-
-    sf::Clock frameClock;
-    main_loop(*window, [&](const sf::Event& event)
+    units::gui::main_loop(*window, [&](float world_x, float world_y)
     {
-        if (event.type == sf::Event::MouseWheelScrolled)
-        {
-            if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
-                main_view.pan_y(event.mouseWheelScroll.delta * 5);
-            else if (event.mouseWheelScroll.wheel == sf::Mouse::HorizontalWheel)
-                main_view.pan_x(event.mouseWheelScroll.delta * 5);
-        }
-        if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
-        {
-            sf::Vector2f world_position = window->mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
-            units::box_formation formation{{1, 0}, {world_position.x, world_position.y}, 1000, 25};
-            assign_formation(formation, group);
-        }
-    },
-    [&]
-    {
-        std::cout << "frame time: " << frameClock.restart().asMicroseconds() << " us" << std::endl;
-        sf::Clock stepClock;
-        update_units(group);
-        std::cout << "update: " << stepClock.restart().asMicroseconds() << " us" << std::endl;
-
-        window->setView(main_view.get_view(window->getSize()));
-        draw_units(group, *window);
-
-        std::cout << "render: " << stepClock.restart().asMicroseconds() << " us" << std::endl;
-    });
-
+        units::box_formation formation{{1, 0}, {world_x, world_y}, 1000, 25};
+        assign_formation(formation, group);
+    }, [&] { update_units(group); }, [&] { units::gui::draw_units(group, *window); } );
 }
