445daaa8edb4b3e846784315a34f95471b67af89
Rafal Przywarski <rafal.przywarski@gmail.com>
2015-12-06 15:54:06 +0100

    states skeleton; removed explicit position

--- source/sim/walking.cpp
+++ source/sim/walking.cpp
@@ -3,31 +3,50 @@
 #include <ams/gui/window.hpp>
 #include <memory>
 
+enum class walking_unit_state
+{
+    STANDING, MOVING_LEFT_LEG, MOVING_RIGHT_LEG
+};
+
 struct walking_unit
 {
     float radius = 0;
     float feet_spacing = 0;
     float speed = 3;
-    ams::vec2f position;
     ams::vec2f left_foot_position, right_foot_position;
     ams::vec2f target_position;
+    walking_unit_state state = walking_unit_state::STANDING;
 
     walking_unit() = default;
-    walking_unit(float radius, float feet_spacing, ams::vec2f position) : radius(radius), feet_spacing(feet_spacing), position(position), left_foot_position(position - ams::vec2f{feet_spacing * 0.5f, 0}), right_foot_position(position + ams::vec2f{feet_spacing * 0.5f, 0}), target_position(position) { }
+    walking_unit(float radius, float feet_spacing, ams::vec2f position) : radius(radius), feet_spacing(feet_spacing), left_foot_position(position - ams::vec2f{feet_spacing * 0.5f, 0}), right_foot_position(position + ams::vec2f{feet_spacing * 0.5f, 0}), target_position(position) { }
+
+    ams::vec2f get_position() const { return 0.5f * (left_foot_position + right_foot_position); }
 };
 
 using walking_units = std::vector<walking_unit>;
 
 void update_unit(walking_unit& u)
 {
-    ams::vec2f direction = u.target_position - u.position;
+    ams::vec2f direction = u.target_position - u.get_position();
     auto distance = length(direction);
     if (distance < 0.5)
+    {
+        u.state = walking_unit_state::STANDING;
         return;
-    u.position += direction * std::min(1.0f, u.speed / distance);
+    }
+    ams::vec2f position = u.get_position() + direction * std::min(1.0f, u.speed / distance);
     direction = direction / distance;
-    u.left_foot_position = u.position + ams::cross_product(direction) * u.feet_spacing * 0.5f;
-    u.right_foot_position = u.position - ams::cross_product(direction) * u.feet_spacing * 0.5f;
+    u.left_foot_position = position + ams::cross_product(direction) * u.feet_spacing * 0.5f;
+    u.right_foot_position = position - ams::cross_product(direction) * u.feet_spacing * 0.5f;
+    u.state = walking_unit_state::MOVING_RIGHT_LEG;
+}
+
+void set_target_position(walking_unit& u, ams::vec2f target_position)
+{
+    if (u.state == walking_unit_state::STANDING)
+    {
+        u.target_position = target_position;
+    }
 }
 
 void draw_circle(sf::CircleShape shape, ams::vec2f pos, float radius, sf::RenderWindow& window)
@@ -49,7 +68,7 @@ void draw_walking_units(const unit_container& group, sf::RenderWindow& window)
 
     for (auto& u : group)
     {
-        draw_circle(shape, u.position, u.radius, window);
+        draw_circle(shape, u.get_position(), u.radius, window);
         float foot_radius = FOOT_RATIO * u.radius;
         draw_circle(shape, u.left_foot_position, foot_radius, window);
         draw_circle(shape, u.right_foot_position, foot_radius, window);
@@ -63,6 +82,6 @@ int main()
 
     ams::gui::main_loop(*window, [&](float world_x, float world_y)
     {
-        unit.target_position = {world_x, world_y};
+        set_target_position(unit, {world_x, world_y});
     }, [&] { update_unit(unit); }, [&] { draw_walking_units(walking_units{unit}, *window); } );
 }
